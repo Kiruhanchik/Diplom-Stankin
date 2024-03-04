ea21a037673096f4e87bb116b9c1291dabcaa219
Kirill <krill.pestun@gmail.com>
2024-03-04 19:58:29 +0300

    Some changes

--- src/components/prepod_connect.jsx
+++ src/components/prepod_connect.jsx
@@ -15,12 +15,15 @@ export default function PrepodConnect() {
  
     const prepods = () => {
         return data.map((prepod) => {
+
+            let href = 'mailto:' + prepod.mail;
+
             return (
                 <div key={prepod.id} id='prepod'>
                     <img src={prepod.photo} alt={prepod.name} />
                     <div id='bio'>
-                        <p id='prepod-name'>{prepod.name}</p>
-                        <p id='prepod-mail'>{prepod.mail}</p>
+                        <p className='prepod-name'>{prepod.name}</p>
+                        <a className='prepod-mail' href={href}>{prepod.mail}</a>
                         <button onClick={() => {setActive(true); setPrepod(prepod); console.log(prepod)}}>Подробнее</button>
                     </div>
                 </div>
